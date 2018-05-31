9e4168e92646155a79589417784131b857d23648
dara <teriaderide@gmail.com>
2018-05-31 16:17:01 +0300

    task3 finished

--- task3.js
+++ task3.js
@@ -1,3 +1,31 @@
 /* Task 3 */
 
-/* Put your code here */
+const sec1 = Number(process.argv[1]);
+const sec2 = Number(process.argv[2]);
+
+const secs =  sec1 + sec2;
+
+const hours = (sec) => (sec - sec%3600) / 3600;
+const minutes = (sec) => {
+    const a = sec - hours(sec)*3600;
+    return (a - a%60) / 60;
+}
+const seconds = (sec) => sec%60;
+
+const word = fun => secs => (str1, str2, str3) => {
+    const num = fun(secs);
+    if (num > 4 && num < 21) return str3;
+    if (num%10 === 1) return str1;
+    if (num%10 > 1 && num%10 < 5) return str2;
+    return str3;
+}
+
+const hoursWord = word(hours)(secs)('час', 'часа', 'часов');
+const minutesWord = word(minutes)(secs)('минута', 'минуты', 'минут')
+const secondsWord = word(seconds)(secs)('секунда', 'секунды', 'секунд')
+
+const str = `${hours(secs)} ${hoursWord} ${minutes(secs)} ${minutesWord} ${seconds(secs)} ${secondsWord}`;
+
+process.stdout.write(str);
+
+/* Put your code here */ 
