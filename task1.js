7bf7e8fc84c39ebd46061f09df7e68d1b4051626
dara <teriaderide@gmail.com>
2018-06-06 23:17:08 +0300

    апгрейд task1

--- task1.js
+++ task1.js
@@ -1,16 +1,29 @@
-/* Task 1 */
 const n = Number(process.argv[2]);
 const e = Number(process.argv[3]);
 
+function addition(a, b, acc = '', carry = 0) {
+  if (!(a.length || b.length || carry)) return acc.replace(/^0+/, '');
+
+  carry = carry + (~~a.pop() + ~~b.pop());
+  acc = carry % 10 + acc;
+  carry = carry > 9;
+
+  return addition(a, b, acc, carry);
+}
+
+function sumStrings(a, b) {
+  if (a === '0' && b === '0') return '0';
+  return addition(a.split(''), b.split(''));
+}
+
 const fun = (num, power) => {
-    let a = 0;
+    let a = '0';
     for (let i = 1; i <= num; i++) {
-        a = a + i ** power;
+        const newA = i ** power;
+        a = sumStrings(a, newA.toString())
+        console.log(a);
     }
     return a;
 }
 
-const stringAnswer = String(fun(n, e));
-
-console.log(stringAnswer);
-/* Put your code here */
+fun(n, e);
